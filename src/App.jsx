81f1314c874bee29ffd14ddd5232ce3b6b26670f
merciof <merciofilho@gmail.com>
2023-02-05 08:55:16 -0300

    add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import QuizzContext from "../context/quizzContext.js";
+import Welcome from "../components/Welcome.jsx";
+import Question from "../components/Question.jsx";
+import GameOver from "../components/GameOver.jsx";
+
+function App() {
+  const [quizzState, dispatch] = React.useContext(QuizzContext);
+
+  return (
+    <div>
+      {quizzState.gameStage === "Start" && <Welcome />}
+      {quizzState.gameStage === "Playing" && <Question />}
+      {quizzState.gameStage === "End" && <GameOver />}
+    </div>
+  );
+}
+
+export default App;
