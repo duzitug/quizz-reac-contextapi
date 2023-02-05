e72ad7205317fb9db6bc05677f1d96f2d2327ba5
merciof <merciofilho@gmail.com>
2023-02-05 08:56:08 -0300

    add questions data

---
+++ data/questions.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    title: "O que é Vanilla JavaScript?",
+    options: [
+      "JavaScript puro",
+      "Uma biblioteca JavaScript",
+      "Um framework JavaScript",
+      "Um compilador de JavaScript",
+    ],
+    answer: "JavaScript puro",
+    url: "https://devkico.itexto.com.br/wp-content/uploads/2017/08/logotipo.png",
+  },
+  {
+    title: "Com qual instrução declaramos uma constante em JavaScript?",
+    options: ["const", "let", "var", "define"],
+    answer: "const",
+    url: "https://bognarjunior.files.wordpress.com/2018/01/1crcyaithv7aiqh1z93v99q.png",
+  },
+  {
+    title: "Qual dos tipos de dado a seguir não existe em JavaScript?",
+    options: ["string", "number", "boolean", "float"],
+    answer: "float",
+    url: "https://a.thumbs.redditmedia.com/zDOFJTXd6fmlD58VDGypiV94Leflz11woxmgbGY6p_4.png",
+  },
+  {
+    title: "Qual dos métodos a seguir seleciona um elemento?",
+    options: ["querySelector", "parseInt", "sort", "reduce"],
+    answer: "querySelector",
+    url: "https://d1v8cmtpnjamtp.cloudfront.net/courses/JD/logo_1633732520417.svg",
+  },
+  {
+    title: "Qual destas propriedades da a quantidade de elementos de um array?",
+    options: ["qty", "length", "items", "index"],
+    answer: "length",
+    url: "https://pt.apkshki.com/storage/3843/icon_5eb57b57eaf4f_3843_w256.png",
+  },
+];
+
+export default data;
