42587b3664191fbb539c164d69846b387641e820
merciof <merciofilho@gmail.com>
2023-02-05 08:55:50 -0300

    add quizzContextProvider

---
+++ context/quizzContextProvider.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import QuizzContext from "./quizzContext.js";
+import questions from "../data/questions.js";
+
+const STAGES = ["Start", "Playing", "End"];
+
+const initialState = {
+  gameStage: STAGES[0],
+  questions,
+  currentQuestion: 0,
+  answer: false,
+};
+
+function quizzReducer(state, action) {
+  switch (action.type) {
+    case "START_GAME":
+      return {
+        ...state,
+        currentQuestion: 0,
+        gameStage: STAGES[1],
+      };
+
+    case "BACK_TO_MENU":
+      return {
+        ...state,
+        gameStage: STAGES[0],
+      };
+
+    case "NEXT_QUESTION":
+      //verificar o fim do array de questões
+      if (state.currentQuestion === questions.length - 1)
+        return {
+          ...state,
+          gameStage: STAGES[2],
+        };
+
+      return {
+        ...state,
+        currentQuestion: state.currentQuestion + 1,
+        answer: false,
+      };
+
+    case "CHECK_ANSWER":
+      //checar se o item selecionado é o correto
+      const x = 10;
+
+      if (state.questions[state.currentQuestion].answer === action.payload)
+        return {
+          ...state,
+          answer: true,
+        };
+
+      return state;
+
+    default:
+      return state;
+  }
+}
+
+function QuizzContextProvider({ children }) {
+  const value = React.useReducer(quizzReducer, initialState);
+
+  return (
+    <QuizzContext.Provider value={value}>{children}</QuizzContext.Provider>
+  );
+}
+
+export default QuizzContextProvider;
