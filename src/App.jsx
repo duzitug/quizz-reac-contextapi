296d476bfb0a9f0d1febe51d2e2b76345ae5dbe7
merciof <merciofilho@gmail.com>
2023-03-06 21:23:52 -0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+
 import QuizzContext from "../context/quizzContext.js";
 import Welcome from "../components/Welcome.jsx";
 import Question from "../components/Question.jsx";
 import GameOver from "../components/GameOver.jsx";
+import Home from "../components/Home.jsx";
+import UserSignIn from "../components/UserSignIn.jsx";
 
 function App() {
   const [quizzState] = React.useContext(QuizzContext);
 
   return (
-    <div>
-      {quizzState.gameStage === "Start" && <Welcome />}
-      {quizzState.gameStage === "Playing" && <Question />}
-      {quizzState.gameStage === "End" && <GameOver />}
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="UserSignIn" element={<UserSignIn />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </HashRouter>
+
+    // <div>
+    //   {quizzState.gameStage === "Start" && <Welcome />}
+    //   {quizzState.gameStage === "Playing" && <Question />}
+    //   {quizzState.gameStage === "End" && <GameOver />}
+    // </div>
   );
 }
 
