5f9d45e74f1fa473553fdd3d8a3608c5f5b22f9d
merciof <merciofilho@gmail.com>
2023-06-26 22:30:32 -0300

    refactor: HashRouter to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import QuizzContext from "../context/quizzContext.js";
 import Welcome from "../components/Welcome.jsx";
@@ -7,17 +7,36 @@ import Question from "../components/Question.jsx";
 import GameOver from "../components/GameOver.jsx";
 import Home from "../components/Home.jsx";
 import UserSignIn from "../components/UserSignIn.jsx";
+import UserSignUp from "../components/UserSignUp.jsx";
+import CreateQuestion from "../components/CreateQuestion.jsx";
+import ListQuestion from "../components/ListQuestion.jsx";
+import LerQuestao from "../components/estudo/LerQestao.jsx";
+import CriarQuestao from "../components/estudo/CriarQuestao.jsx";
+import Hero from "../components/Hero.jsx";
+import Estrutura from "../components/Estrutura.jsx";
+import Login from "../components/Login.jsx";
 
 function App() {
   const [quizzState] = React.useContext(QuizzContext);
 
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="UserSignIn" element={<UserSignIn />} />
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Hero />} /> */}
+
+        <Route path="/" element={<Estrutura />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/userSignIn" element={<UserSignIn />} />
+        <Route path="/userSignUp" element={<UserSignUp />} />
+        <Route path="/createQuestion" element={<CreateQuestion />} />
+        <Route path="/listQuestion" element={<ListQuestion />} />
+
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/" element={<CriarQuestao />} /> */}
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
 
     // <div>
     //   {quizzState.gameStage === "Start" && <Welcome />}
