531bdb9c077ea32950f4d9b43b41c19b1e04e35f
merciof <merciofilho@gmail.com>
2023-06-27 21:53:33 -0300

    refactor

--- components/Estrutura.jsx
+++ components/Estrutura.jsx
@@ -1,35 +1,10 @@
+import { Outlet } from "react-router-dom";
 import { Layout, Space } from "antd";
 import Cabecalho from "./Header.jsx";
 import Hero from "./Hero.jsx";
 import ArticlePreviewCard from "./CartaoPreVisualizacaoArtigo.jsx";
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Footer, Content } = Layout;
 
-const headerStyle = {
-  textAlign: "center",
-  color: "#fff",
-  height: 64,
-  paddingInline: 50,
-  lineHeight: "64px",
-  backgroundColor: "#7dbcea",
-};
-const contentStyle = {
-  textAlign: "center",
-  minHeight: 120,
-  lineHeight: "120px",
-  color: "#fff",
-  backgroundColor: "#108ee9",
-};
-const siderStyle = {
-  textAlign: "center",
-  lineHeight: "120px",
-  color: "#fff",
-  backgroundColor: "#3ba0e9",
-};
-const footerStyle = {
-  textAlign: "center",
-  color: "#fff",
-  backgroundColor: "#7dbcea",
-};
 const Estrutura = () => (
   <Space
     direction="vertical"
@@ -44,15 +19,7 @@ const Estrutura = () => (
       </Header>
       <Layout>
         <Content style={{ backgroundColor: "white" }}>
-          <Hero />
-
-          <ArticlePreviewCard
-            title={"Titulo do artigo"}
-            date={"dezembro"}
-            description={"descricap"}
-            author={"autor"}
-            image={"https://th.bing.com/th/id/OIP.nhyWEOmF_nmKhIg9LY5U8wAAAA"}
-          />
+          <Outlet />
         </Content>
       </Layout>
       <Footer style={{ backgroundColor: "white" }}>Rodapé</Footer>
