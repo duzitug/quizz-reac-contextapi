8a08c86fa5d409213c21a8ae4acdcdb3a70ce7d2
merciof <merciofilho@gmail.com>
2023-10-04 19:26:37 -0300

    criar: componente CriarArtigo

---
+++ components/estudo/CriarArtigo.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+import axios from "axios";
+
+function CriarArtigo() {
+  const [artigo, mudarArtigo] = React.useState({
+    titulo: "titulo",
+    corpo: "corpo",
+    slug: "slug",
+    descricao: "descricao",
+  });
+
+  const criarArtigo = async () => {
+    const resposta = await axios({
+      url: "http://localhost:3001/api/artigo",
+      method: "POST",
+      data: { artigo },
+      headers: {
+        autorizacao:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1vbmljYUBlbWFpbC5jb20iLCJpYXQiOjE2OTUwNzY2MzN9.V10XgMQlR7jeOICcl3ZP_jzAgI-98HGX0LEqlH_XqHc",
+      },
+    });
+  };
+
+  return (
+    <div>
+      <h1>Componente CriarArtigo</h1>
+
+      <h1 onClick={criarArtigo}>Clique para Criar</h1>
+
+      <input
+        type="text"
+        placeholder="titulo"
+        value={artigo.titulo}
+        onChange={(e) => mudarArtigo({ ...artigo, titulo: e.target.value })}
+      />
+
+      <input
+        type="text"
+        placeholder="corpo"
+        value={artigo.corpo}
+        onChange={(e) => mudarArtigo({ ...artigo, corpo: e.target.value })}
+      />
+
+      <input
+        type="text"
+        placeholder="slug"
+        value={artigo.slug}
+        onChange={(e) => mudarArtigo({ ...artigo, slug: e.target.value })}
+      />
+
+      <input
+        type="text"
+        placeholder="descricao"
+        value={artigo.descricao}
+        onChange={(e) => mudarArtigo({ ...artigo, descricao: e.target.value })}
+      />
+    </div>
+  );
+}
+
+export default CriarArtigo;
